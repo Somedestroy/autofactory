ecd9a9e4ab7a64fc95552c38d8cce1f1ccb21a09
Somedestroy <somebody.max@yandex.ru>
2021-11-05 16:26:07 +0300

    Add all my files

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using PatternAbstractFactoryProj.Factory;
+
+namespace PatternAbstractFactoryProj
+{
+    class Program
+    {
+        enum AutoBrand
+        {
+            Audi, // 0
+            BMW, // 1
+            Mercedes // 2
+        }
+        static void Main(string[] args)
+        { 
+            Console.WriteLine(@"Please write auto brand (Audi = 0\BMW = 1\Mercedes = 2):");
+            int autoBrand = Int32.Parse(Console.ReadLine());
+            while (autoBrand < 0 || autoBrand > 2)
+            {
+                Console.WriteLine("Please write again");
+                autoBrand = Int32.Parse(Console.ReadLine());
+            }
+
+            AutoFactory autoFactory;
+            switch ((AutoBrand)autoBrand)
+            {
+                case AutoBrand.Audi:
+                    autoFactory = new AudiAutoFactory();
+                    break;
+                case AutoBrand.BMW:
+                    autoFactory = new BMWAutoFactory();
+                    break;
+                case AutoBrand.Mercedes:
+                    autoFactory = new MercedesAutoFactory();
+                    break;
+                default:
+                    autoFactory = new AudiAutoFactory();
+                    break;
+            }
+
+            var newSedan = autoFactory.CreateSedan();
+            Console.WriteLine($"New Sedan created: Sedan Type - {newSedan.GetType()}");
+        }
+    }
+}
